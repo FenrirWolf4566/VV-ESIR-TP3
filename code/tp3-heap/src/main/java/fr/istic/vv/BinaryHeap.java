9c86c71861322db2747ec18d0e1e3a553675bd23
fgdou <fabigoardou@gmail.com>
2022-12-07 11:46:31 +0100

    Advance Heap

--- code/tp3-heap/src/main/java/fr/istic/vv/BinaryHeap.java
+++ code/tp3-heap/src/main/java/fr/istic/vv/BinaryHeap.java
@@ -1,17 +1,69 @@
 package fr.istic.vv;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 class BinaryHeap<T> {
 
-    public BinaryHeap(Comparator<T> comparator) {  }
+    List<T> array;
+    Comparator<T> comparator;
 
-    public T pop() { return null; }
 
-    public T peek() { return null; }
+    public BinaryHeap(Comparator<T> comparator) {
+        array = new ArrayList<>();
+        this.comparator = comparator;
+    }
 
-    public void push(T element) {  }
+    public T pop() {
+        int index = getMinElementIndex();
 
-    public int count() { return 0; }
+        swap(0, index);
+        return null; // TODO
+    }
+
+    public T peek() { return array.get(getMinElementIndex()); }
+
+    private int getMinElementIndex(){
+        if(array.isEmpty())
+            throw new NoSuchElementException();
+        int index = 0;
+        while(getChild1Index(index) < array.size())
+            index = getChild1Index(index);
+        return index;
+    }
+
+    public void push(T element) {
+        array.add(element);
+
+        int child = array.size()-1;
+        int parent = getParentIndex(child);
+
+        while(comparator.compare(array.get(parent), array.get(child)) < 0){
+            swap(parent, child);
+
+            child = parent;
+            parent = getParentIndex(child);
+        }
+    }
+
+    private void swap(int a, int b){
+        T tmp = array.get(a);
+        array.set(a, array.get(b));
+        array.set(b, tmp);
+    }
+
+    public int count() { return array.size(); }
+
+    private static int getChild1Index(int i){
+        return 2*i+1;
+    }
+    private static int getChild2Index(int i){
+        return 2*i+2;
+    }
+    private static int getParentIndex(int i){
+        return (i-1)/2;
+    }
 
 }
